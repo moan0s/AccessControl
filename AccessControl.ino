26f8ca3840c6eabff8e9a04c7e72268b1d65eccb
Sebastian <mail@sebastian-ludwig.name>
2021-09-22 21:54:07 +0200

    First preparations

---
+++ AccessControl.ino
@@ -0,0 +1,30 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         4          // Change to your configuration
+#define SS_PIN          15         // Change to your configuration
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+void setup() {
+  Serial.begin(9600);   // Initialize serial communications with the PC
+  SPI.begin();          // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+}
+
+void loop()
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+
+   // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+}
