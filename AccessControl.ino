600371272980dce469e81abb2f8b08a4f555ddda
Sebastian <mail@sebastian-ludwig.name>
2021-09-27 22:35:27 +0200

    Basic Wifi configuration

--- AccessControl.ino
+++ AccessControl.ino
@@ -1,17 +1,32 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <ESP8266WiFi.h>
 
 #define RST_PIN         4          // Change to your configuration
 #define SS_PIN          15         // Change to your configuration
 
+char* ssid     = "DEMO-WIFI";      // Change to your configuration
+char* password = "16x7<Z82";       // Change to your configuration
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
 void setup() {
-  Serial.begin(9600);   // Initialize serial communications with the PC
-  SPI.begin();          // Init SPI bus
+  Serial.begin(115200);   // Initialize serial communications with the PC
+  SPI.begin();            // Init SPI bus
   mfrc522.PCD_Init();   // Init MFRC522
   mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
-  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  Serial.println(WiFi.status());
+  while (WiFi.status() != WL_CONNECTED) {
+
+    //WiFi.begin(ssid, password);
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop()
@@ -37,5 +52,4 @@ void loop()
   String cardnumber = content.substring(1);
   cardnumber.replace(" ", "");  //Remove spaces
   Serial.println(cardnumber);
-
 }
